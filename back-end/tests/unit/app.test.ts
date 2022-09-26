0e7f1e69cd1e17afb28502e622cfdefa1ddc9588
Isabela <isabela_cmoreira@hotmail.com>
2022-09-26 16:14:14 -0300

    feat: created unit tests

--- back-end/tests/unit/app.test.ts
+++ back-end/tests/unit/app.test.ts
@@ -1,7 +1,8 @@
 import { jest } from "@jest/globals";
 import {
   createRecommendation,
-  createManyRecommendations,
+  createRecommendationWithId,
+  createTenRecommendations,
 } from "../factories/scenarioFactory";
 import { recommendationService } from "../../src/services/recommendationsService";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository";
@@ -11,21 +12,99 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-describe("recommendation unit test suite", () => {
+describe("tests route POST '/recommendation'", () => {
   it("should create a recommendation", async () => {
-    const recommendation = {
-      name: faker.lorem.words(5),
-      youtubeLink: "https://www.youtube.com/watch?v=0vq3qZwaXrw",
-    };
-
     jest
       .spyOn(recommendationRepository, "findByName")
       .mockImplementationOnce((): any => {});
-
     jest
       .spyOn(recommendationRepository, "create")
       .mockImplementationOnce((): any => {});
+    const data = await createRecommendation();
+    await recommendationService.insert(data);
+    expect(recommendationRepository.findByName).toBeCalled();
+    expect(recommendationRepository.create).toBeCalled();
+  });
+  it("should not allow the creation of a recommendation that already exists", async () => {
+    const data = await createRecommendation();
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementationOnce((): any => {
+        return data;
+      });
+    const result = recommendationService.insert(data);
+    expect(result).rejects.toEqual({
+      message: "Recommendations names must be unique",
+      type: "conflict",
+    });
+  });
+});
 
-    await recommendationService.insert(recommendation.name);
+describe("tests route POST /recommendations/:id/upvote", () => {
+  it("should upvote a recommendation", async () => {
+    const data = await createRecommendationWithId();
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(data);
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockResolvedValueOnce(data);
+    await recommendationService.upvote(data.id);
+    expect(recommendationRepository.updateScore).toHaveBeenCalled();
+  });
+
+  it("should not upvote a recommendation that is invalid", async () => {
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null);
+    const promise = recommendationService.upvote(0);
+    expect(promise).rejects.toEqual({ message: "", type: "not_found" });
+  });
+});
+
+describe("tests route POST /recommendations/:id/downvote", () => {
+  it("should downvote a recommendation", async () => {
+    const data = await createRecommendationWithId();
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(data);
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockResolvedValueOnce(data);
+    await recommendationService.downvote(data.id);
+    expect(recommendationRepository.updateScore).toHaveBeenCalled();
+  });
+
+  it("should not downvote a recommendation that is invalid", async () => {
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null);
+    const promise = recommendationService.downvote(0);
+    expect(promise).rejects.toEqual({ message: "", type: "not_found" });
+  });
+});
+
+describe("tests route GET '/recommendations'", () => {
+  it("should get recommendations", async () => {
+    const data = await createTenRecommendations();
+    jest.spyOn(recommendationRepository, "findAll").mockResolvedValueOnce(data);
+    await recommendationService.get();
+    expect(recommendationRepository.findAll).toHaveBeenCalled();
+  });
+});
+
+describe("tests route GET'/recommendations/:id'", () => {
+  it("should get a recommendation by its id", async () => {
+    const data = await createRecommendationWithId();
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(data);
+    await recommendationService.getById(data.id);
+    expect(recommendationRepository.find).toHaveBeenCalled();
+  });
+});
+
+describe("tests route GET '/recommendations/random'", () => {
+  it("should get random recommendations", async () => {
+    const data = await createTenRecommendations();
+    jest.spyOn(recommendationRepository, "findAll").mockResolvedValueOnce(data);
+    await recommendationService.getRandom();
+    expect(recommendationRepository.getAmountByScore).toHaveBeenCalled();
+  });
+
+  it("should not return random recommendations", async () => {
+    jest.spyOn(recommendationRepository, "findAll").mockResolvedValueOnce([]);
+    const promise = recommendationService.getRandom();
+    expect(promise).rejects.toEqual({ message: "", type: "not_found" });
   });
 });
